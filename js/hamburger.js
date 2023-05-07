1bbbb65fbaf41aebfca588e08c19456cab347aa5
RahymCharyev <r569837@gmail.com>
2023-05-07 14:09:06 +0500

    🐞 fix: Fixed hamburger menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,29 @@
-let wrapperMenu = document.querySelector("#menu");
-let menuLinks = document.querySelector(".menu-links");
-wrapperMenu.addEventListener("click", function () {
-  wrapperMenu.classList.toggle("open");
-  menuLinks.classList.toggle("open");
+let navLinks;
+let burgerIcon = document.querySelector(".burger-icon");
+
+document.addEventListener("DOMContentLoaded", function () {
+  navLinks = document.querySelector(".nav-links");
+
+  function toggleMenu() {
+    navLinks.classList.toggle("show");
+    burgerIcon.classList.toggle("active");
+  }
+
+  burgerIcon.addEventListener("click", toggleMenu);
+
+  const navLinksList = document.querySelectorAll(".nav-links a");
+  navLinksList.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      closeMenu();
+      setTimeout(function () {
+        window.location = link.href;
+      }, 800);
+    });
+  });
 });
+
+function closeMenu() {
+  navLinks.classList.remove("show");
+  burgerIcon.classList.remove("active");
+}
